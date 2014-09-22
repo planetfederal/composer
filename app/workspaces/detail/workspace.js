f83cbb050b7ab92a5dbd09b3274d38bd6a0aee4d
Eva Shon <eshon@boundlessgeo.com>
2014-09-22 19:11:06 -0400

    Revert "Restructing a little"
    This reverts commit 20e763b66715dba49b7c26b416c4444c5fb391b1.

--- app/workspaces/detail/workspace.js
+++ app/workspaces/detail/workspace.js
@@ -1,9 +1,5 @@
 angular.module('gsApp.workspaces.workspace', [
-  'gsApp.workspaces.workspace.maps',
-  'gsApp.workspaces.workspace.data',
-  'ngGrid',
-  'ngSanitize',
-  'ui.router'
+  'ngGrid', 'ngSanitize', 'ui.router',
 ])
 .config(['$stateProvider', '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
@@ -17,37 +13,27 @@ angular.module('gsApp.workspaces.workspace', [
         })
         .state('workspace.home.maps', {
           url: '/maps',
-          templateUrl: '/workspaces/detail/workspace-maps/workspace-maps.tpl.html',
-          controller: 'WorkspaceMapsCtrl'
+          templateUrl: '/workspaces/detail/workspace-maps.tpl.html'
         })
         .state('workspace.home.data', {
           url: '/data',
-          templateUrl: '/workspaces/detail/workspace-data/workspace-data.tpl.html',
-          controller: 'WorkspaceDataCtrl'
+          templateUrl: '/workspaces/detail/workspace-data.tpl.html'
         });
     }])
 .controller('WorkspaceHomeCtrl', ['$scope', '$stateParams',
-  'GeoServer', '$log', '$state', '$timeout',
-    function($scope, $stateParams, GeoServer, $log, $state,
-      $timeout) {
-
-      var wsName = $stateParams.workspace;
-      $scope.title = wsName;
+  'GeoServer', '$log', '$sce', 'baseUrl', '$window', '$state',
+    function($scope, $stateParams, GeoServer, $log, $sce, baseUrl,
+      $window, $state) {
 
       // Allow tabs in url
       $scope.tabs = [
         { heading: 'Maps', route:'workspace.home.maps', active:false },
         { heading: 'Data', route:'workspace.home.data', active:false }
       ];
-      $scope.go = function(route) {
-        if ($scope.timer) {
-          $timeout.cancel($scope.timer);
-        }
-        $scope.timer = $timeout((function() {
-            $state.go(route);
-        }), 200);
+      $scope.go = function(route){
+        $state.go(route);
       };
-      $scope.active = function(route) {
+      $scope.active = function(route){
         return $state.is(route);
       };
       $scope.$on('$stateChangeSuccess', function() {
@@ -55,6 +41,84 @@ angular.module('gsApp.workspaces.workspace', [
             tab.active = $scope.active(tab.route);
         });
       });
+
+      var wsName = $stateParams.workspace;
+      $scope.title = wsName;
+      $scope.thumbnails = {};
+      $scope.olmaps = {};
+
+      // Maps
+
+      GeoServer.maps.get({workspace: wsName}).$promise
+        .then(function(maps) {
+
+          $scope.maps = maps;
+
+          // load all map thumbnails & metadata
+          for (var i=0; i < $scope.maps.length; i++) {
+            var map = $scope.maps[i];
+            var layers = '';
+
+            $scope.maps[i].workspace = wsName;
+            $scope.maps[i].layergroupname = wsName + ':' + map.name;
+            $scope.maps[i].layerCount = map.layers.length;
+            var bbox = $scope.maps[i].bbox = '&bbox=' + map.bbox.west +
+             ',' + map.bbox.south + ',' + map.bbox.east + ',' +
+             map.bbox.north;
+
+            var url = GeoServer.map.thumbnail.get(map.workspace, map,
+              map.layergroupname, 250, 250);
+            var srs = '&srs=' + map.proj.srs;
+
+            $scope.thumbnails[map.name] = baseUrl + url + bbox +
+              '&format=image/png' + srs;
+          }
+        });
+
+      $scope.sanitizeHTML = function(description) {
+        return $sce.trustAsHtml(description);
+      };
+
+      $scope.newOLWindow = function(map) {
+        var baseUrl = GeoServer.map.openlayers.get(map.workspace,
+          map.name, map.bbox, 800, 500);
+        $window.open(baseUrl);
+      };
+
+      $scope.onEdit = function(map) {
+        $state.go('map.compose', {
+          workspace: map.workspace,
+          name: map.name
+        });
+      };
+
+      // Data
+
+      $scope.datastores = GeoServer.datastores.get().datastores; // stub
+
+      $scope.pagingOptions = {
+        pageSizes: [25, 50, 100],
+        pageSize: 25
+      };
+      $scope.gridOptions = {
+        data: 'datastores',
+        columnDefs: [
+          {field: 'workspace', displayName: 'Workspace'},
+          {field: 'store', displayName: 'Store'},
+          {field: 'type', displayName: 'Data Type'},
+          {field: 'source', displayName: 'Source', width: '30%'},
+          {field: 'description', displayName: 'Description', width: '20%'},
+          {field: 'srs', displayName: 'SRS'}
+        ],
+        enablePaging: true,
+        enableColumnResize: false,
+        showFooter: true,
+        pagingOptions: $scope.pagingOptions,
+        filterOptions: {
+          filterText: '',
+          useExternalFilter: true
+        }
+      };
 
     }]);
 
