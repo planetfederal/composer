52ab3883985ef5e90e83ca2e5ab7297c4c948528
jdeolive <jdeolive@boundlessgeo.com>
2014-10-03 09:30:28 -0600

    Fixing protractor config to point to selenium under grunt-protractor-runner module

--- protractor.js
+++ protractor.js
@@ -2,6 +2,6 @@ exports.config = {
   specs: ['app/**/*.spec.js'],
   keepAlive: true,
   seleniumServerJar: 
-    'node_modules/protractor/selenium/selenium-server-standalone-2.43.1.jar',
-  chromeDriver: 'node_modules/protractor/selenium/chromedriver'
+    'node_modules/grunt-protractor-runner/node_modules/protractor/selenium/selenium-server-standalone-2.43.1.jar',
+  chromeDriver: 'node_modules/grunt-protractor-runner/node_modules/protractor/selenium/chromedriver'
 }
