3df2c6ab6896951df823da8e6c2596bf5af286b2
Robert Denis <rdenis@boundlessgeo.com>
2014-10-24 16:01:02 -0700

    Custom footer for grid and other updates, merges #696.
    - Altering backend.js layer.update from PUT to PATCH per Jody's suggestion
    - Altering edit functionality of layer and map titles within the ngGrid
    - Adding minor pieces to the home page.

--- app/home/home.js
+++ app/home/home.js
@@ -10,13 +10,21 @@ angular.module('gsApp.home', [])
     }])
 .controller('HomeCtrl', ['$scope', 'GeoServer',
     function($scope, GeoServer) {
-      $scope.title = 'Summary';
+      $scope.title = 'Home';
       GeoServer.serverInfo.get().$promise.then(function(serverInfo) {
         $scope.serverInfo = serverInfo;
         $scope.server = serverInfo.server;
+        //$scope.serverInfo = serverInfo;
+
         if (!$scope.server) {
           $scope.server = {};
         }
+
+        if (serverInfo.title) { $scope.server.title = serverInfo.title; }
+        else { $scope.server.title = 'localhost:8000'; }
+        //$scope.server.status = serverInfo.status;
+        $scope.server.status = 'ok';
+        
         $scope.server.url = GeoServer.baseUrl();
         $scope.catalog = serverInfo.catalog;
         $scope.services = serverInfo.services;
