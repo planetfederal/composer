739442373e77d731b08ee1d8f9c542f202cbdf43
jdeolive <jdeolive@boundlessgeo.com>
2014-10-29 14:18:21 -0700

    Changing geoserver/app to geoserver/composer and geoserver/app/api to geoserver/api.

--- src/main/java/com/boundlessgeo/geoserver/AppDispatcher.java
+++ src/main/java/com/boundlessgeo/geoserver/AppDispatcher.java
@@ -3,14 +3,18 @@
  */
 package com.boundlessgeo.geoserver;
 
+import org.geoserver.ows.util.ResponseUtils;
 import org.springframework.web.context.support.ServletContextResource;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerExecutionChain;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.List;
@@ -23,6 +27,9 @@ import java.util.List;
  */
 public class AppDispatcher extends DispatcherServlet {
 
+    static final String API_PATH = "/api";
+    static final String COMPOSER_PATH = "/composer";
+
     ResourceHttpRequestHandler appHandler;
 
     @Override
@@ -30,20 +37,20 @@ public class AppDispatcher extends DispatcherServlet {
         super.init(config);
         appHandler = new ResourceHttpRequestHandler();
 
-        ServletContextResource appRoot = new ServletContextResource(config.getServletContext(), "/app/**");
+        ServletContextResource composerRoot = new ServletContextResource(config.getServletContext(), "/composer/**");
         appHandler.setApplicationContext(getWebApplicationContext());
-        appHandler.setLocations((List)Arrays.asList(appRoot));
+        appHandler.setLocations((List)Arrays.asList(composerRoot));
 
     }
 
     @Override
     protected void doDispatch(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        if (request.getServletPath().startsWith("/app") && !request.getPathInfo().startsWith("/api")) {
+        if (request.getServletPath().startsWith(COMPOSER_PATH) && !request.getPathInfo().startsWith(API_PATH)) {
             String path = request.getPathInfo();
 
             // handle redirect
             if ("".equals(path)) {
-                response.sendRedirect("app/");
+                response.sendRedirect(COMPOSER_PATH.substring(1) + "/");
                 return;
             }
 
@@ -59,5 +66,22 @@ public class AppDispatcher extends DispatcherServlet {
         else {
             super.doDispatch(request, response);
         }
+    }
+
+    @Override
+    protected HandlerExecutionChain getHandler(HttpServletRequest request) throws Exception {
+        String servletPath = request.getServletPath();
+        if (servletPath != null && servletPath.startsWith(API_PATH)) {
+            //JD: Hack to deal with the GeoServer "advanced dispatch", the point here is that we want to expose
+            // the rest api directly "/api"
+            request = new HttpServletRequestWrapper(request) {
+                @Override
+                public String getRequestURI() {
+                    return super.getRequestURI().replace(API_PATH, API_PATH + API_PATH);
+                }
+            };
+        }
+
+        return super.getHandler(request);
     }
 }
