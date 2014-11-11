76ee0961193bf085f6b4d50a3c86c97dfaeca96a
Eva Shon <eshon@boundlessgeo.com>
2014-11-11 15:57:39 -0500

    Adding keyboard shortcuts info to composer page

---
+++ app/components/styleditor/tools/shortcuts.js
@@ -0,0 +1,20 @@
+/*global window, document, ZeroClipboard, $ */
+angular.module('gsApp.styleditor.shortcuts', [])
+.controller('ShortcutsCtrl', ['$scope', '$modalInstance',
+    function($scope, $modalInstance) {
+
+      $scope.close = function () {
+        $modalInstance.close();
+      };
+
+      $scope.chooseIcon = function(iconname) {
+        $scope.selectedIconName = iconname;
+      };
+
+      $scope.shortcuts = [
+        { 'name': 'Save', 'keys': 'Ctrl-S'},
+        { 'name': 'Autocomplete', 'keys': 'Ctrl-Spc'},
+        { 'name': 'Code fold/unfold', 'keys': 'Ctrl-F'},
+        { 'name': 'Select line', 'keys': 'Shift-Up/Down'}
+      ];
+    }]);
